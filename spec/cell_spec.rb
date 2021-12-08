677894b79cf6000f602cec5478b0cbf7f90b291e
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-07 20:08:54 -0600

    Test: add tests for #render - returns value based upon conditional

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -54,7 +54,67 @@ RSpec.describe Cell do
     expect(cell.fired_upon?).to be true
   end
 
+  it '7* will render not fired upon to begin' do
+    cell_1 = Cell.new("B4")
 
+    expect(cell_1.render).to eq(".")
+  end
 
+  it '8* can render "M" when fired upon and shot is a miss' do
+    cell_1 = Cell.new("B4")
+
+    expect(cell_1.render).to eq(".")
+
+    cell_1.fire_upon
+
+    expect(cell_1.render).to eq("M")
+  end
+
+  it '9* can render "H" when fired upon and shot is a hit' do
+    cell_1 = Cell.new("B4")
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+
+    expect(cell_2.render).to eq(".")
+
+    cell_2.fire_upon
+
+    expect(cell_2.render).to eq("H")
+    expect(cruiser.sunk?).to be false
+  end
+
+  it '10* can render "x" when fired upon and ship has sunk' do
+    cell_1 = Cell.new("B4")
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+
+    expect(cell_2.render).to eq(".")
+
+    cell_2.fire_upon
+
+    expect(cell_2.render).to eq("H")
+    expect(cruiser.sunk?).to be false
+
+    cruiser.hit
+    cruiser.hit
+
+    expect(cruiser.sunk?).to be true
+    expect(cell_2.render).to eq("X")
+  end
+
+  # it '11* can render "S" when given optional boolean' do
+  #   cell_1 = Cell.new("B4")
+  #   cell_2 = Cell.new("C3")
+  #   cruiser = Ship.new("Cruiser", 3)
+  #
+  #   cell_2.place_ship(cruiser)
+  #   expect(cell_2.render).to eq(".")
+  #
+  #   expect(cell_2.render(true)).to eq("S")
+  # end
 
 end
