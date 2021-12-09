46289e80ef8632454de32d0de419b584917ff8c6
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-09 15:23:50 -0600

    Feat: add #render to display Board to user.

--- lib/board.rb
+++ lib/board.rb
@@ -33,4 +33,12 @@ def get_starting_board
       end
     end
   end
+
+  def render(option = nil)
+    return "  1 2 3 4 \n" +
+    "A #{@cells["A1"].render(option)} #{@cells["A2"].render(option)} #{@cells["A3"].render(option)} #{@cells["A4"].render(option)} \n" +
+    "B #{@cells["B1"].render(option)} #{@cells["B2"].render(option)} #{@cells["B3"].render(option)} #{@cells["B4"].render(option)} \n" +
+    "C #{@cells["C1"].render(option)} #{@cells["C2"].render(option)} #{@cells["C3"].render(option)} #{@cells["C4"].render(option)} \n" +
+    "D #{@cells["D1"].render(option)} #{@cells["D2"].render(option)} #{@cells["D3"].render(option)} #{@cells["D4"].render(option)} \n"
+  end
 end
