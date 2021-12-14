03fa007c099e97fe07f536e158bb148dd4d387a9
Tamara Dowis <tamara.dowis@gmail.com>
2021-12-14 15:18:13 -0600

    test: initialize 2 ships player and computer

--- lib/battle.rb
+++ lib/battle.rb
@@ -2,74 +2,6 @@ class Battle
   attr_reader :computer_board, :player_board, :player_ships, :computer_ships
 
   def initialize
-<<<<<<< HEAD
-    @board = Board.new
-    @ships = get_ships
-  end
-
-  def get_ships
-    @ships = []
-    @ships << Ship.new("Cruiser", 3)
-    @ships << Ship.new("Submarine", 2)
-    @ships << Ship.new("Carrier", 5)
-    @ships << Ship.new("Battleship", 4)
-    @ships << Ship.new("Destroyer", 2)
-    return @ships
-  end
-
-until ships
-#   def get_coordinates
-#     gets.chomp.split(" ")
-#   end
-#
-# # Get's info from player and places Cruiser
-# # ISSUES: Cruiser is hardcoded.
-#   def player_place_cruiser
-#     cruiser = Ship.new("Cruiser", 3)
-#     selected_coordinates = get_coordinates
-#
-#     until @board.valid_placement?(cruiser, selected_coordinates) == true
-#       p "Those are invalid coordinates. Please try again:"
-#       get_coordinates
-#     end
-#     @board.place(cruiser, selected_coordinates)
-#   end
-#
-#   # Get's info from player and places Submarine
-#   # ISSUES: Submarine is hardcoded.
-#   def player_place_submarine
-#     submarine = Ship.new("Submarine", 2)
-#     selected_coordinates = get_coordinates
-#
-#     until @board.valid_placement?(submarine, selected_coordinates) == true
-#       p "Those are invalid coordinates. Please try again:"
-#       get_coordinates
-#     end
-#     @board.place(submarine, selected_coordinates)
-#   end
-#
-#   # THIS IS DESIGNED TO REPLACE THE HARDCODED SHIPS FOR THE PLAYER_PLACE METHOD
-#   def player_place_ship(ship)
-#     selected_coordinates = get_coordinates
-#
-#     until @board.valid_placement?(ship, selected_coordinates) == true
-#       p "Those are invalid coordinates. Please try again:"
-#       get_coordinates
-#     end
-#     @board.place(ship, selected_coordinates)
-#   end
-#
-# # MAJOR WORK IN PROGRESS. THIS WOULD BE THE COMPUTER'S PLACEMENT OF THE SHIPS
-# # ALSO CURRENTLY HARDCODED
-#   def computer_place_cruiser
-#     cruiser = Ship.new("Cruiser", 3)
-#     available_coordinates = @board
-#     selected_coordinates = []
-#
-#     selected_coordinates << available_coordinates.sample(cruiser.length)
-#     until @board.valid_placement?
-#   end
-=======
     @computer_board = Board.new
     @player_board = Board.new
     @player_ships = get_starting_ships
@@ -78,7 +10,7 @@ until ships
 
   def get_starting_ships
     available_ships = [cruiser = Ship.new("Cruiser", 3), submarine = Ship.new("Submarine", 2)]
-    available_ships.sample(2)
+    # available_ships.sample(2)
   end
 
   def start
@@ -186,10 +118,78 @@ until ships
       end
     end
   end
->>>>>>> 303a60cd1f0eca3b9aeea934a969b4cf8580c900
 
   def test_runner
     @computer_board.render(true)
     @player_board.render(true)
   end
 end
+
+
+#
+#     @board = Board.new
+#     @ships = get_ships
+#   end
+#
+#   def get_ships
+#     @ships = []
+#     @ships << Ship.new("Cruiser", 3)
+#     @ships << Ship.new("Submarine", 2)
+#     @ships << Ship.new("Carrier", 5)
+#     @ships << Ship.new("Battleship", 4)
+#     @ships << Ship.new("Destroyer", 2)
+#     return @ships
+#   end
+#
+# until ships
+#   def get_coordinates
+#     gets.chomp.split(" ")
+#   end
+#
+# # Get's info from player and places Cruiser
+# # ISSUES: Cruiser is hardcoded.
+#   def player_place_cruiser
+#     cruiser = Ship.new("Cruiser", 3)
+#     selected_coordinates = get_coordinates
+#
+#     until @board.valid_placement?(cruiser, selected_coordinates) == true
+#       p "Those are invalid coordinates. Please try again:"
+#       get_coordinates
+#     end
+#     @board.place(cruiser, selected_coordinates)
+#   end
+#
+#   # Get's info from player and places Submarine
+#   # ISSUES: Submarine is hardcoded.
+#   def player_place_submarine
+#     submarine = Ship.new("Submarine", 2)
+#     selected_coordinates = get_coordinates
+#
+#     until @board.valid_placement?(submarine, selected_coordinates) == true
+#       p "Those are invalid coordinates. Please try again:"
+#       get_coordinates
+#     end
+#     @board.place(submarine, selected_coordinates)
+#   end
+#
+#   # THIS IS DESIGNED TO REPLACE THE HARDCODED SHIPS FOR THE PLAYER_PLACE METHOD
+#   def player_place_ship(ship)
+#     selected_coordinates = get_coordinates
+#
+#     until @board.valid_placement?(ship, selected_coordinates) == true
+#       p "Those are invalid coordinates. Please try again:"
+#       get_coordinates
+#     end
+#     @board.place(ship, selected_coordinates)
+#   end
+#
+# # MAJOR WORK IN PROGRESS. THIS WOULD BE THE COMPUTER'S PLACEMENT OF THE SHIPS
+# # ALSO CURRENTLY HARDCODED
+#   def computer_place_cruiser
+#     cruiser = Ship.new("Cruiser", 3)
+#     available_coordinates = @board
+#     selected_coordinates = []
+#
+#     selected_coordinates << available_coordinates.sample(cruiser.length)
+#     until @board.valid_placement?
+#   end
--- spec/battle_spec.rb
+++ spec/battle_spec.rb
@@ -7,7 +7,7 @@ require './lib/battle'
 RSpec.describe Battle do
   it '1* exists' do
     battle = Battle.new
-    require 'pry'; binding.pry
+    # require 'pry'; binding.pry
     expect(battle).to be_instance_of(Battle)
   end
 
@@ -18,12 +18,21 @@ RSpec.describe Battle do
     expect(battle.player_board).to be_instance_of(Board)
   end
 
-  it '3* the computer can place ships on the board' do
+  it '3* initializes with 2 ship arrays' do
+    battle = Battle.new
+
+    expect(battle.player_ships[0].name).to eq("Cruiser")
+    expect(battle.player_ships[1].name).to eq("Submarine")
+    expect(battle.computer_ships[0].name).to eq("Cruiser")
+    expect(battle.computer_ships[1].name).to eq("Submarine")
+  end
+
+  it '4* the computer can place ships on the board' do
     battle = Battle.new
     battle.computer_place_ship
   end
 
-  it '4* the player can place ships on the board' do
+  it '5* the player can place ships on the board' do
     battle = Battle.new
     # battle.computer_place_ship
     # battle.player_place_ship
