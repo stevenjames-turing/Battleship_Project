5b8460ddcc42f457e4c58708fcbb806ad6f14f08
Tamara Dowis <tamara.dowis@gmail.com>
2021-12-15 15:13:44 -0600

    refactored code and added comments

--- lib/ship.rb
+++ lib/ship.rb
@@ -7,11 +7,15 @@ class Ship
     @health = length
     @hit_counter = 0
   end
-
+ # When initialized the ship's health == it's size. It's health
+ # decreases by one each time it's hit. When the health , 1 the ship
+ # will be sunk. 
   def sunk?
     @health < 1
   end
 
+# When the cell the ship in sitting in is hit,
+# the number of hits on the ship +1 and the health -1
   def hit
     @health -= 1
     @hit_counter += 1
